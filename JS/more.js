bf2a9914fe285d20fc87f47b970a2956a2b35963
Nikolay <trefalin01@gmail.com>
2021-05-11 22:02:38 +0300

    Still here

---
+++ JS/more.js
@@ -0,0 +1,11 @@
+const btnMore = document.querySelectorAll('.content-item_more'); 
+
+function edit(btnMore) {
+    for (let i = 0; i < btnMore.length; i++) {
+        const btn = btnMore[i];
+        
+    }
+    
+}
+
+edit(btnMore);
